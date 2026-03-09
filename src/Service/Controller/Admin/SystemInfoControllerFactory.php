5f547dd3b9f51145398207057a67f15afd9b8652
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-09 00:00:00 +0000

    Replaced Interop\Container by standard Psr\Container.

--- src/Service/Controller/Admin/SystemInfoControllerFactory.php
+++ src/Service/Controller/Admin/SystemInfoControllerFactory.php
@@ -2,7 +2,7 @@
 
 namespace Vips\Service\Controller\Admin;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Vips\Controller\Admin\SystemInfoController;
 
--- src/Service/File/Thumbnailer/VipsCliFactory.php
+++ src/Service/File/Thumbnailer/VipsCliFactory.php
@@ -2,7 +2,7 @@
 
 namespace Vips\Service\File\Thumbnailer;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Vips\File\Thumbnailer\VipsCli;
 
